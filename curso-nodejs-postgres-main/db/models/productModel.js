43a330adea3fe81b9d145b7e4734a630372ac6a2
Alejandro Manzo <nanommfp@gmail.com>
2022-11-08 17:07:03 +0100

    modelo en sequelizr de products

---
+++ curso-nodejs-postgres-main/db/models/productModel.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+
+const PRODUCT_TABLE = 'product';
+
+const ProductSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+  nombre: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    unique: false,
+  },
+  price: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    unique: false,
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'create_at',
+    defaultValue: Sequelize.NOW
+  }
+}
+
+class Product extends Model {
+  static associate() {
+    // associate
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: PRODUCT_TABLE,
+      modelName: 'Product',
+      timestamps: false
+    }
+  }
+}
+
+
+module.exports = { PRODUCT_TABLE, ProductSchema, Product }
